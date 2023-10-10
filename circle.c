a1f5350a8ae3a4a5a90a179737f503c8081a2d2d
alekhya178 <147472858+alekhya178@users.noreply.github.com>
2023-10-10 11:23:46 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -2,8 +2,7 @@
 // area of a cicle
 int main()
 {
-	int radius;// does it matter if we use int or float here 
-	// i identified radius as a whole number here but when i enter the value of r as 1.55 why did i get the answer.
+	int radius;
 	printf("enter radius");
 	scanf("%d",&radius);
 	printf(" area of :%f",3.13* radius * radius);
